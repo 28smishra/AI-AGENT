3ce854140cd4c6b08d2c623cb39f892583f0a188
Shashank <shashank.m.mishra@oracle.com>
2022-06-22 17:33:26 +0530

    Initial Commit

--- src/main/java/core_framework/WebElementUtils.java
+++ src/main/java/core_framework/WebElementUtils.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
